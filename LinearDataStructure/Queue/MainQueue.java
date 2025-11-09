3776d6312391a1ee3ed6bac3caa608bc36ca8a26
Arijit Sinhamahapatra <85805003+ArijitSMP@users.noreply.github.com>
2025-11-09 18:21:36 +0530

    Implement main queue operations in MainQueue.java

---
+++ LinearDataStructure/Queue/MainQueue.java
@@ -0,0 +1,78 @@
+package Queue;
+
+import java.util.Queue;
+import java.util.Scanner;
+
+import Stack.StackError;
+
+public class MainQueue {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of Queue");
+		QueueProgram q=new QueueProgram(sc.nextInt());
+		boolean flag=true; 
+		
+		while(flag) {
+			System.out.println("***Welcome to Queue*****");
+			System.out.println("1.Enqueue\n2.Dequeue\n3.Peek\n4.isEmpty?\n5.isFull?\n6.Display\n7.Exit");
+			System.out.println("*********");
+			int ch = sc.nextInt();
+			switch(ch)
+			{
+				case 1: System.out.print("Enter the data :"); int data = sc.nextInt();
+						try
+						{
+							q.enqueue(data);
+							System.err.println(q);
+						}
+						catch(StackError e)
+						{
+							System.err.println(e.getMessage());
+						}
+						
+						break;
+						
+				case 2 : try 
+						{
+							q.dequeue();
+							System.err.println(q);
+						}
+						catch(StackError e)
+						{
+								System.err.println(e.getMessage());	
+						}
+				
+						break;
+						
+				case 3 :  try 
+						{
+							System.out.println(q.peek());
+						}
+						catch(StackError e)
+						{
+								System.err.println(e.getMessage());	
+						}
+				
+						break;
+				
+				case 4 : System.out.println(q.isEmpty() ? "stack is empty" : "stack is not empty");
+						 break;
+				
+				case 5 : System.out.println(q.isFull() ? "stack is full" : "stack is not full");
+				
+						break;
+						
+				case 6 : System.err.println(q); break;
+				
+				case 7 : flag = false; break;
+				
+				default : System.err.println("Invalid option");
+				
+		}
+		
+		}		
+
+	}
+
+}
